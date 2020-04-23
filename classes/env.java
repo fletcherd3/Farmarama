2b42ec876dc8ecea265b024d07e65db65ed1cf05
Fletcher Dick <fletcher_dick@y.lan>
2020-04-23 13:55:42 +1200

    Made methods for the Farmer object and create game env class

---
+++ classes/env.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class env {
+
+    public static void main(String[] args) {
+
+        Farmer player = new Farmer();
+        player.setName("Fletcher");
+        System.out.println(player.getName());
+
+      }
+
+}
